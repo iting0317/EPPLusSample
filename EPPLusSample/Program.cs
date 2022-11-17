fbf6820a2287fb9fe5e123fdb358921ea3e40de2
tim.wu <tim.wu@ecpay.com.tw>
2022-11-17 15:11:55 +0800

    1. 使用Top-Level Statements寫法 2. Readme增加環境資訊

--- EPPLusSample/Program.cs
+++ EPPLusSample/Program.cs
@@ -1,32 +1,23 @@
-﻿using System.Text;
+﻿using EPPLusSample;
 
-namespace EPPLusSample
+XLSHelper xlsHeler = new XLSHelper();
+
+string path = @"..\..\..\測試匯入資料.xlsx";
+string fullPath = Path.Combine(Directory.GetCurrentDirectory(), path);
+
+var valid = xlsHeler.ValidFile(fullPath);
+if (!valid.IsValid)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            XLSHelper xlsHeler = new XLSHelper();
-
-            string path = @"..\..\..\測試匯入資料.xlsx";
-            string fullPath = Path.Combine(Directory.GetCurrentDirectory(), path);
-
-            var valid = xlsHeler.ValidFile(fullPath);
-            if (!valid.IsValid)
-            {
-                Console.WriteLine(valid.Msg);
-                Console.ReadKey();
-                Environment.Exit(0);
-            }
-
-            var fileStream = File.OpenRead(fullPath);
-            
-            //讀取為List<string>
-            var stringResult = xlsHeler.ReadExcelToStringList(fileStream);
-            stringResult.ForEach(line => Console.WriteLine(line));
-
-            //讀取為DataTable
-            var datatableResult = xlsHeler.ReadExcelToDataTable(fileStream);
-        }
-    }
+    Console.WriteLine(valid.Msg);
+    Console.ReadKey();
+    Environment.Exit(0);
 }
+
+var fileStream = File.OpenRead(fullPath);
+            
+//讀取為List<string>
+var stringResult = xlsHeler.ReadExcelToStringList(fileStream);
+stringResult.ForEach(line => Console.WriteLine(line));
+
+//讀取為DataTable
+var datatableResult = xlsHeler.ReadExcelToDataTable(fileStream);
--- Readme.md
+++ Readme.md
@@ -1,3 +1,6 @@
+# 環境
+.NET 6 Console Application
+
 # 使用EPPlus套件讀取excel檔案範例
 
 ReadExcelToStringList(): 將Excel讀取為csv字串
